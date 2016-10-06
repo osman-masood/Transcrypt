1ef16832f8a4ac8895033731a2d3a8409a750cfe
osman-masood <oamasood@gmail.com>
2016-10-06 15:59:48 -0700

    ES2015-style React component, WIP
    -Converting HelloComponent into ES2015 style
    -Also adding helper methods for div(), p(), and h1() to be a bit cleaner than using h().

--- transcrypt/demos/react_demo/react_demo.py
+++ transcrypt/demos/react_demo/react_demo.py
@@ -1,8 +1,38 @@
+__pragma__('skip')
+document = window = Math = Date = 0  # Prevent complaints by optional static checker
+__pragma__('noskip')
+
+
 # Helper functions
 
 
 def h(elm_type, props='', *args):
+    """
+    :param elm_type: str
+    :param str|dict|None props: Props
+    """
     return React.createElement(elm_type, props, *args)
+
+
+def h1(props='', *args):
+    """
+    :param str|dict|None props: Props
+    """
+    return h('h1', props, *args)
+
+
+def p(props='', *args):
+    """
+    :param str|dict|None props: Props
+    """
+    return h('p', props, *args)
+
+
+def div(props='', *args):
+    """
+    :param str|dict|None props: Props
+    """
+    return h('div', props, *args)
 
 
 def render(react_element, destination_id, callback=lambda: None):
@@ -12,25 +42,65 @@ def render(react_element, destination_id, callback=lambda: None):
 
 # Create a component
 
+def getInitialState():
+    return {'counter': 0}
 
-Hello = React.createClass({
-    'displayName': 'Hello',
 
-    'getInitialState': lambda: {'counter': 0},
+#### FROM ORIGINAL EXAMPLE -- WORKS ####
+# Hello = React.createClass({
+#     'displayName': 'Hello',
+#
+#     'getInitialState': getInitialState,
+#
+#     'updateCounter': lambda: (this.setState({'counter': str([1, 2, 3] == ',,')})),
+#
+#     'componentDidMount': lambda: (setInterval(this.updateCounter, 1000)),
+#
+#     'render': lambda: div({'className': 'maindiv'},
+#                           h1(None, 'Hello', this.props['name']),
+#                           p(None, 'Lorem ipsum dolor sit ame.'),
+#                           p(None, 'Counter: ', this.state['counter'])
+#                           )
+# })
 
-    'updateCounter': lambda: (this.setState({'counter': this.state['counter']+1})),
+####  DOESNT WORK ####
+class Hello(React.Component):
+    displayName = 'Hello'
 
-    'componentDidMount': lambda: (setInterval(this.updateCounter, 1000)),
+    def __init__(self, **props):
+        self.state = {'counter': 0}
+        self.props = props
 
-    'render': lambda: h('div', {'className': 'maindiv'},
-                          h('h1', None, 'Hello ', this.props['name']),
-                          h('p', None, 'Lorem ipsum dolor sit ame.'),
-                          h('p', None, 'Counter: ', this.state['counter'])
-                        )
-})
+    def updateCounter(self):
+        self.state['counter'] += 1
+
+    def componentDidMount(self):
+        setInterval(self.updateCounter, 1000)
+
+    def render(self):
+        return div({'className': 'maindiv'},
+                   h1('Hello', self.props['name']),
+                   p('Lorem ipsum dolor sit ame.'),
+                   p('Counter: ', self.state['counter'])
+                   )
 
 
 # Render the component in a 'container' div
 
 element = React.createElement(Hello, {'name': 'React!'})
 render(element, 'container')
+
+
+# if __name__ == "__main__":
+#     """
+#     TODO: Transpile HelloComponent into ES5-style syntax.
+#
+#     1) class HelloComponent(React.Component):  ->  React.createClass({
+#
+#     2) self -> this
+#
+#     3) __init__ constructor -> getInitialState
+#
+#     4) (Optional) HTML elements -> h() thing. If an arg is a dict, should be a prop.
+#     """
+#     pass
